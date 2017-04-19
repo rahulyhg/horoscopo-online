a97e138e2dfe2287b6ae6037c844db694640c199
vzaffalon <zaffalonvictor@gmail.com>
2017-04-18 23:22:07 -0300

    main page controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main_page/index'
+
   resources :weekly_horoscopes
   resources :signs
   resources :perfils
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   resources :combinations
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'main_page#index'
 end
